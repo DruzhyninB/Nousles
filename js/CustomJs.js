c6ea5afb6eb031e3f18008c805a998437540fa36
DruzhyninB <druzhyninbogdan@gmail.com>
2015-06-18 23:22:39 +0300

    изм6

---
+++ js/CustomJs.js
@@ -0,0 +1,30 @@
+﻿//Documentation
+
+//1.Scrolling Parallax
+//2.Smooth Scroll
+
+
+// 1.Scrolling Parallax
+// Using jquery.scrolling-parallax.js
+$.scrollingParallax('img/body-dg.png', {
+    enableHorizontal: true,
+    bgWidth : '200%'
+});
+
+//2.Smooth Scroll
+
+//$(document).bind('mousewheel', function (e) {
+//    var nt = $(document.body).scrollTop() - (e.deltaY * e.deltaFactor * 100);
+//    e.preventDefault();
+//    e.stopPropagation();
+//    $(document.body).stop().animate({
+//        scrollTop: nt
+//    }, 500, 'easeInOutCubic');
+//})
+$(window).load(function () {
+    $("body").mCustomScrollbar({
+        theme:"dark",
+        axis: "y",
+        scrollbarPosition: "outside"
+    });
+});
