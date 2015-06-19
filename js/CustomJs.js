e1f56730b1bff21a349b1e6e4fb1ece0572d2ceb
DruzhyninB <druzhyninbogdan@gmail.com>
2015-06-19 04:37:48 +0300

    bpvty7

--- js/CustomJs.js
+++ js/CustomJs.js
@@ -1,30 +1,108 @@
 ﻿//Documentation
 
 //1.Scrolling Parallax
-//2.Smooth Scroll
+//2.Service logic
 
 
 // 1.Scrolling Parallax
 // Using jquery.scrolling-parallax.js
+
 $.scrollingParallax('img/body-dg.png', {
     enableHorizontal: true,
     bgWidth : '200%'
 });
 
-//2.Smooth Scroll
+//2.Service logic
 
-//$(document).bind('mousewheel', function (e) {
-//    var nt = $(document.body).scrollTop() - (e.deltaY * e.deltaFactor * 100);
-//    e.preventDefault();
-//    e.stopPropagation();
-//    $(document.body).stop().animate({
-//        scrollTop: nt
-//    }, 500, 'easeInOutCubic');
-//})
-$(window).load(function () {
-    $("body").mCustomScrollbar({
-        theme:"dark",
-        axis: "y",
-        scrollbarPosition: "outside"
-    });
-});
+window.addEventListener("load", start, false);
+
+function start() {
+    document.getElementById("hexa2").addEventListener("click", hex2Click, true);
+    document.getElementById("hexa3").addEventListener("click", hex3Click, true);
+    document.getElementById("hexa4").addEventListener("click", hex4Click, true);
+    document.getElementById("hexa5").addEventListener("click", hex5Click, true);
+    document.getElementById("hexa6").addEventListener("click", hex6Click, true);
+    document.getElementById("cicle-group-info").addEventListener("click", CircleExpand, true);
+
+    function CircleExpand(e) {
+        var elem = e.target;
+        var closeIcon = document.createElement("div");
+        closeIcon.className = "closeIcon";
+        closeIcon.innerHTML = "X"
+        if (elem.className !== "closeIcon") {
+            if (elem.querySelector("#active-circle .closeIcon") == null){elem.id = "active-circle";
+            elem.appendChild(closeIcon);
+            elem.querySelector("#active-circle .closeIcon").style.opacity = 1;
+            } else {
+                //здесь код с занесение отметки в список чек боксов
+        }
+            
+        } else {
+            elem.parentElement.id = ""
+            elem.parentElement.removeChild(elem);
+
+        }
+    }
+
+    function hex2Click(e) {
+        var elements = document.getElementsByClassName("filter-brand");
+        if (elements[1].style.display == "block") {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "none";
+            }
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                        elements[i].style.display = "block";
+                    }
+        }
+        
+    }
+    function hex3Click(e) {
+        var elements = document.getElementsByClassName("filter-webInter");
+        if (elements[1].style.display == "block") {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "none";
+            }
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "block";
+            }
+        }
+    }
+    function hex4Click(e) {
+        var elements = document.getElementsByClassName("filter-manegment");
+        if (elements[1].style.display == "block") {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "none";
+            }
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "block";
+            }
+        }
+    }
+    function hex5Click(e) {
+        var elements = document.getElementsByClassName("filter-marketing");
+        if (elements[1].style.display == "block") {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "none";
+            }
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "block";
+            }
+        }
+    }
+    function hex6Click(e) {
+        var elements = document.getElementsByClassName("filter-contGener");
+        if (elements[1].style.display == "block") {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "none";
+            }
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.display = "block";
+            }
+        }
+    }
+};
---
+++ js/FormJS.js
@@ -0,0 +1,2 @@
+﻿//config list of check-box
+
